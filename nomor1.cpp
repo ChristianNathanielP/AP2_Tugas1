83a81eac56892edd6bca610ac0348bbc6dae27c4
irwanpurba231401115 <irwanpurba2019@gmail.com>
2024-03-25 23:26:34 +0700

    Tugas1_115_AP2

---
+++ nomor1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    int angka;
+
+    cout << "Masukkan angka anda " << endl;
+    cin >> angka;
+
+    if (angka % 2 == 0){
+        cout << angka << " merupakan bilangan ganjil" << endl;
+    }
+    else
+    {
+        cout << angka << " merupakan bilangan ganjil" << endl;
+    }
+}
